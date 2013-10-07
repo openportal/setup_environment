482fef5aba9a420c2acdaafb675fe4ed0ec46805
Robert Hintz <roberthintz@gmail.com>
2013-10-07 05:59:21 +0000

    Add starting file

---
+++ ubuntu_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install vim
+
+sudo apt-get install git-core
+
+git config --global user.name "Robert Hintz"
+git config --global user.email "roberthintz@stanford.edu"
+
+sudo apt-get install curl
+
+curl -L https://get.rvm.io | bash -s stable --rails
+
+source /home/ubuntu/.rvm/scripts/rvm
+
+if [[ -a ~/dotfiles || -d ~/dotfiles ]]
+  then
+    rm -rf ~/dotfiles
+fi
+
+git clone https://github.com/rhintz42/dotfiles.git ~/dotfiles
+
+cd ~/dotfiles
+
+bash ~/dotfiles/setup.sh
+
+cd ~
