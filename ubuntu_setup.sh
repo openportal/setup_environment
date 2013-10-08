d1da8802225690aa17dc4cd1ea0323086b1f3939
Robert Hintz <roberthintz@gmail.com>
2013-10-08 11:55:48 -0700

    Add --force-yes to all apt-gets

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-sudo apt-get -y update
-sudo apt-get -y upgrade
+sudo apt-get -y --force-yes update
+sudo apt-get -y --force-yes upgrade
 
-sudo apt-get -y install vim
+sudo apt-get -y --force-yes install vim
 
-sudo apt-get -y install git-core
+sudo apt-get -y --force-yes install git-core
 
 git config --global user.name "Robert Hintz"
 git config --global user.email "roberthintz@stanford.edu"
 
-sudo apt-get -y install curl
+sudo apt-get -y --force-yes install curl
 
-sudo apt-get -y install nginx-full
+sudo apt-get -y --force-yes install nginx-full
 
-sudo apt-get -y install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
+sudo apt-get -y --force-yes install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
 
-sudo apt-get -y install libpq-dev build-essential
+sudo apt-get -y --force-yes install libpq-dev build-essential
 
-sudo apt-get -y install linux-headers-3.2.0-23-generic build-essential
+sudo apt-get -y --force-yes install linux-headers-3.2.0-23-generic build-essential
 
-sudo apt-get -y install ruby1.9.1-dev
+sudo apt-get -y --force-yes install ruby1.9.1-dev
 
-sudo apt-get -y install curl git-core python-software-properties build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libcurl4-openssl-dev
+sudo apt-get -y --force-yes install curl git-core python-software-properties build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libcurl4-openssl-dev
 
 sudo rm /etc/nginx/sites-enabled/default
 
