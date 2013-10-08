2b6c787c0be58ca221d440c0a666b558166ca7e0
Robert Hintz <roberthintz@stanford.edu>
2013-10-08 18:39:56 +0000

    Add the -y option to apt-gets

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
-sudo apt-get install vim
+sudo apt-get -y install vim
 
-sudo apt-get install git-core
+sudo apt-get -y install git-core
 
 git config --global user.name "Robert Hintz"
 git config --global user.email "roberthintz@stanford.edu"
 
-sudo apt-get install curl
+sudo apt-get -y install curl
 
-sudo apt-get install nginx-full
+sudo apt-get -y install nginx-full
 
-sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
+sudo apt-get -y install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
 
-sudo apt-get install libpq-dev build-essential
+sudo apt-get -y install libpq-dev build-essential
 
-sudo apt-get install linux-headers-3.2.0-23-generic build-essential
+sudo apt-get -y install linux-headers-3.2.0-23-generic build-essential
 
-sudo apt-get install ruby1.9.1-dev
+sudo apt-get -y install ruby1.9.1-dev
 
 sudo apt-get -y install curl git-core python-software-properties build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libcurl4-openssl-dev
 
